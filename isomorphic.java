b99ea1e6a92ef84c62af9163089ff3d7efc9a474
Abe Vega <avega15@calstatela.edu>
2020-07-07 20:54:34 -0700

    added comments

--- isomorphic.java
+++ isomorphic.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class isomorphic{
 	public static void main(String[] args) {
 	
-	
+		//Just test cases. First one passes and second fails
 		String first = "css";
 		String second = "dll";
 		
@@ -34,9 +34,12 @@ public class isomorphic{
 	}
 
 	public static boolean isIsomorphic(String input1, String input2) {
+		//We figure out if two strings are isomorphic by using a hashmap.
+		//Keep count of each letter and check if both hashmaps are the same.
 		HashMap<Character, Integer> map1 = new HashMap<Character, Integer>();
 		HashMap<Character, Integer> map2 = new HashMap<Character, Integer>();
 		
+		//Populates both hashmaps
 		for(int i = 0; i < input1.length(); i++){
 			if(map1.containsKey(input1.charAt(i))){
 				map1.put(input1.charAt(i), map1.get(input1.charAt(i)) + 1);
@@ -51,7 +54,8 @@ public class isomorphic{
 			}
 		}
 		
-		
+		//Checks count of each item in hashmaps.
+		//If there isn't a match then it is not isomorphic.
 		for(int i = 0; i < input1.length(); i++){
 			if(map1.get(input1.charAt(i)) != map2.get(input2.charAt(i))){
 				return false;
